cd0aaeeac4de15e907dfe7a6b3c00337cac7c4d1
JMC <JMC@DESKTOP-O4044JL>
2020-12-10 19:34:42 +0100

    Finalizado

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,12 +1,52 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="curso.examen.m3.libreria.Util" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Examen M3 - Nombre Apellido1 Apellido2</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<meta charset="ISO-8859-1">
+	<title>Examen M3 - Jose Miguel Cabanas Garcia</title>
 </head>
 <body>
+<%ArrayList<String> provincias = Util.obtenerListadoProvincias();%>
+<%Boolean enviado = (Boolean) request.getAttribute("enviado"); %>
 
+<div class="jumbotron text-center">
+	<h1>Examen M3</h1>
+	<p>Jose Miguel Cabanas Garcia</p>
+</div>
+
+<div class="container">
+	<form id="mailForm" class="form-group" action="<% request.getContextPath();%>" method="post">
+		<label>Nombre:</label><input class="form-control" type="text" name="nombre" placeholder="Nombre destinatario" required><br>
+		<label>Correo:</label><input class="form-control" type="email" name="correo" placeholder="Email destinatario" maxlength="40" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required><br>
+		<label>Provincia:</label>
+		<select class="form-control" name="provincia">
+			<%if(provincias != null){
+				for(String txt:provincias){%>
+					<option><%=txt%></option>
+				<%}
+			}%>
+		</select><br>
+		<label>Asunto:</label><input class="form-control" type="text" name="asunto" placeholder="Asunto"><br>
+		<label>Mensaje:</label><textarea class="form-control" rows="5" name="mensaje" placeholder="Mensaje" required></textarea><br>
+		<input class="form-control" type="submit" name="botonEnviar" value="Enviar">
+	</form><br>
+		
+	<%if(enviado != null){ 
+		if(enviado){%>
+			<div class="alert alert-success">
+				<p>Mensaje enviado con éxito.</p>
+			</div>
+		<%}else{ %>
+			<div class="alert alert-warning">
+				<p>Algo salió mal, el mensaje no se envió.</p>
+			</div>
+		<%}%>
+	<%}%>
+	
+</div>
 </body>
 </html>
--- src/curso/examen/m3/Controlador.java
+++ src/curso/examen/m3/Controlador.java
@@ -1,6 +1,9 @@
 package curso.examen.m3;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +12,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.*;
+import org.apache.log4j.helpers.Loader;
+
+import curso.examen.m3.db.dao.SQLContactoDAO;
+import curso.examen.m3.db.modelo.Contacto;
+import curso.examen.m3.libreria.Util;
+
 /**
  * Servlet implementation class Controlador
  */
 @WebServlet("")
 public class Controlador extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static Logger log = Logger.getLogger(Logger.class);
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,7 +38,6 @@ public class Controlador extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: completar
 		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 		rd.forward(request, response);
 	}
@@ -36,9 +46,51 @@ public class Controlador extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: completar
+		PropertyConfigurator.configure(Loader.getResource("log4j.properties"));
+		
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    	URL appResourceURL = loader.getResource("email.properties");
+    	String propertiesFileRoute = appResourceURL.getPath();
+    	propertiesFileRoute = propertiesFileRoute.substring(1, propertiesFileRoute.length());
+    	
+		Properties properties = new Properties();
+		properties.load(new FileReader(propertiesFileRoute));
+		
+		String emailUser = properties.getProperty("emailUser");
+		String passUser = properties.getProperty("passUser");
+		
+		String nombre = request.getParameter("nombre");
+		String emailDestino = request.getParameter("correo");
+		String provincia = request.getParameter("provincia");
+		String asuntoEmail = request.getParameter("asunto");
+		String cuerpoEmail = request.getParameter("mensaje");
+		
+		Boolean enviado = null;
+		
+		if(emailDestino!=null && asuntoEmail!=null && cuerpoEmail!=null) {
+			SQLContactoDAO scd = new SQLContactoDAO();
+			Contacto c = new Contacto();
+			
+			c.setNombre(nombre);
+			c.setCorreo(emailDestino);
+			c.setProvincia(provincia);
+			c.setAsunto(asuntoEmail);
+			c.setMensaje(cuerpoEmail);
+			
+			enviado = Util.enviarEmail(emailUser, passUser, emailDestino, asuntoEmail, cuerpoEmail);
+			if(enviado) {
+				c.setEnviado("Si");
+				request.setAttribute("enviado", true);
+				log.info("Mensaje enviado correctamente");
+			}else{
+				c.setEnviado("No");
+				request.setAttribute("enviado", false);
+				log.warn("No se ha podido enviar el mensaje");
+			}
+			scd.setContacto(c);
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 		rd.forward(request, response);
 	}
-
 }
